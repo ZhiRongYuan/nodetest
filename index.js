a68a673e75927b1f9293a0b90fdb637e8161fa6f
Richard <Richard@RicharddeMacBook-Pro.local>
2017-12-17 17:04:25 +0800

    modify

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<h1>Node.js</h1>');
-    res.end('<p>Hello World������</p>');
+    res.end('<p>Hello World! 袁志荣</p>');
 }).listen(3389);
 console.log("HTTP server is listening at port 3389.");
